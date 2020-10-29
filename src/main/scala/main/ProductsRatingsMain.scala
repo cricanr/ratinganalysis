e7db8ca4e1ef8032dafb8efb78f9685baad43b0b
Radu Crican <cricanr@gmail.com>
2020-10-29 20:06:22 +0100

    Code reorganization around Product Service

--- src/main/scala/main/ProductsRatingsMain.scala
+++ src/main/scala/main/ProductsRatingsMain.scala
@@ -6,16 +6,17 @@ import modules.Module
 import net.codingwell.scalaguice.InjectorExtensions._
 import services.IProductsService
 
-object ProductsRatingsMain extends Greeting with App with LazyLogging{
+object ProductsRatingsMain extends Greeting with App with LazyLogging {
   private val injector = Guice.createInjector(new Module())
   private val productsService = injector.instance[IProductsService]
 
   def printProductsRatingsSummaryJson(): Unit = {
-    val productsRaw = productsService.getRawProducts
-    val productsRatingsSummary = productsService.getProductsRatingsSummary(productsRaw)
+    val eitherProductsRatingsSummary = productsService.getProductsRatingsSummary
 
-    val productsRatingsSummaryJson = s"${productsService.getProductsRatingsSummaryAsJson(productsRatingsSummary)}"
-    println(s"Products ratings summary: $productsRatingsSummaryJson")
+    eitherProductsRatingsSummary.foreach { productsRatingsSummary =>
+      val productsRatingsSummaryJson = s"${productsService.getProductsRatingsSummaryAsJson(productsRatingsSummary)}"
+      println(s"Products ratings summary: $productsRatingsSummaryJson")
+    }
   }
 
   printProductsRatingsSummaryJson()
--- src/main/scala/services/ProductsService.scala
+++ src/main/scala/services/ProductsService.scala
@@ -7,54 +7,50 @@ import io.circe.syntax.EncoderOps
 import models.{Product, ProductValidator, ProductsRatingsSummary}
 
 trait IProductsService {
-  def getRawProducts: List[List[String]]
-
   def getProducts(productsRaw: List[List[String]]): Seq[Either[Object, Product]]
 
-  def getProductsRatingsSummary(productsRaw: List[List[String]]): ProductsRatingsSummary
+  def getProductsRatingsSummary: Either[Throwable, ProductsRatingsSummary]
 
   def getProductsRatingsSummaryAsJson(productsRatingsSummary: ProductsRatingsSummary): String
 }
 
 class ProductsService @Inject()(productParser: IProductParser) extends IProductsService {
+  override def getProductsRatingsSummary: Either[Throwable, ProductsRatingsSummary] = {
+    val eitherProductsRaw = productParser.readAllProducts()
+    eitherProductsRaw.map { productsRaw =>
+      val eitherProducts = getProducts(productsRaw)
+      val validProducts: Seq[Product] = eitherProducts.collect { case Right(product) => product }
 
-  override def getRawProducts: List[List[String]] = {
-    productParser.readAllProducts().getOrElse(List.empty)
-  }
+      val countValidProducts = validProducts.size
+      val countInvalidProducts = eitherProducts.count(product => product.isLeft)
 
-  override def getProductsRatingsSummary(productsRaw: List[List[String]]): ProductsRatingsSummary = {
-    val eitherProducts = getProducts(productsRaw)
-    val validProducts: Seq[Product] = eitherProducts.collect { case Right(product) => product }
+      val productsByProductId = validProducts.groupBy(product => product.productId)
+      val productsWithAvgRating = productsByProductId.map {
+        case (key, value) => key -> value.foldLeft(0)(_ + _.rating) / value.size
+      }
+      val productsWithAvgRatingSortedDesc = productsWithAvgRating.toSeq.sorted
 
-    val countValidProducts = validProducts.size
-    val countInvalidProducts = eitherProducts.count(product => product.isLeft)
+      val bestRatedProducts = productsWithAvgRatingSortedDesc.takeRight(3).map(_._1)
+      val worstRatedProducts = productsWithAvgRatingSortedDesc.take(3).map(_._1)
 
-    val productsByProductId = validProducts.groupBy(product => product.productId)
-    val productsWithAvgRating = productsByProductId.map {
-      case (key, value) => key -> value.foldLeft(0)(_ + _.rating) / value.size
+      val productsWithRatingCount = productsByProductId.map { case (key, value) =>
+        key -> value.size
+      }
+
+      val productsWithSortedRatingCount = productsWithRatingCount.toSeq.sorted
+
+      val mostRatedProducts = productsWithSortedRatingCount.takeRight(3).map(_._1)
+      val lessRatedProducts = productsWithSortedRatingCount.take(3).map(_._1)
+
+      val productsRatingsSummary = ProductsRatingsSummary(countValidProducts,
+        countInvalidProducts,
+        bestRatedProducts,
+        worstRatedProducts,
+        mostRatedProducts,
+        lessRatedProducts)
+
+      productsRatingsSummary
     }
-    val productsWithAvgRatingSortedDesc = productsWithAvgRating.toSeq.sorted
-
-    val bestRatedProducts = productsWithAvgRatingSortedDesc.takeRight(3).map(_._1)
-    val worstRatedProducts = productsWithAvgRatingSortedDesc.take(3).map(_._1)
-
-    val productsWithRatingCount = productsByProductId.map { case (key, value) =>
-      key -> value.size
-    }
-
-    val productsWithSortedRatingCount = productsWithRatingCount.toSeq.sorted
-
-    val mostRatedProducts = productsWithSortedRatingCount.takeRight(3).map(_._1)
-    val lessRatedProducts = productsWithSortedRatingCount.take(3).map(_._1)
-
-    val productsRatingsSummary = ProductsRatingsSummary(countValidProducts,
-      countInvalidProducts,
-      bestRatedProducts,
-      worstRatedProducts,
-      mostRatedProducts,
-      lessRatedProducts)
-
-    productsRatingsSummary
   }
 
   def getProducts(productsRaw: List[List[String]]): Seq[Either[Object, Product]] = {
